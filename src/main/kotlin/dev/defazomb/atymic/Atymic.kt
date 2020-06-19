aa975f53648d861ac79050cab276351825dbed81
defazomb <me@defazomb.dev>
2020-06-19 01:05:49 -0700

    Register a block

--- src/main/kotlin/dev/defazomb/atymic/Atymic.kt
+++ src/main/kotlin/dev/defazomb/atymic/Atymic.kt
@@ -1,7 +1,29 @@
 package dev.defazomb.atymic
 
+import net.minecraft.block.Block
+import net.minecraft.block.material.Material
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.ForgeRegistries
+import org.apache.logging.log4j.LogManager
 
-@Mod("atymic")
+@Mod(Atymic.MOD_ID)
 class Atymic {
+
+    init {
+        BLOCKS.register(FMLJavaModLoadingContext.get().modEventBus)
+    }
+
+    companion object {
+
+        const val MOD_ID = "atymic"
+
+        private val LOGGER = LogManager.getLogger(MOD_ID)
+
+        private val BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID)
+        private val ROCK_BLOCK = BLOCKS.register("rock") {
+            Block(Block.Properties.create(Material.ROCK))
+        }
+    }
 }
